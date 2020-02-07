fa369dca101d9e4c0ba2c9b5d11b5f4c9ce95bde
ryotaro.saji <abcabcxxx01@gmail.com>
2020-02-07 17:59:28 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -20,10 +21,12 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
+  end
+    
     
     private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  end
+  
 end
